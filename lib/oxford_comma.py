021b6268567fe447e1f0388af55a4c91d07c9611
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-18 23:13:01 -0700

    fun lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,19 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 2:
+        string = " and ".join(items)
+    elif len(items) == 3:
+        string = items[0] + ", " + items[1] + ", and " + items[-1]
+    elif len(items) > 3:
+        string = ", ".join(items[:-1]) + ", and " + items[-1]
+    else:
+        string = items[0]
+    return string
+
+    # string = " ".join(items)
+
+    # for 2 items:
+    #     item1 and item2
+
+    # for items == 3:
+    #     item1, item2, and item3
+ 
